9742c449694698296bad8e20f41db4a43ee02012
dohayman <124599378+DohaAyman55@users.noreply.github.com>
2025-08-27 17:25:10 +0300

    Update start.js

--- node-tutorial/myapp/start.js
+++ node-tutorial/myapp/start.js
@@ -1,35 +1,35 @@
 const express = require('express');
-const passport = require('passport');
+// const passport = require('passport');
 const bodyParser = require('body-parser');
-const xsenv = require('@sap/xsenv');
-const { createSecurityContext, requests, constants, TokenInfo, JWTStrategy } = require("@sap/xssec").v3;
+// const xsenv = require('@sap/xsenv');
+// const { createSecurityContext, requests, constants, TokenInfo, JWTStrategy } = require("@sap/xssec").v3;
 
 const users = require('./users.json');
 const app = express();
 
-const services = xsenv.getServices({ uaa: 'nodeuaa' });
+// const services = xsenv.getServices({ uaa: 'nodeuaa' });
 
-passport.use(new JWTStrategy(services.uaa));
+// passport.use(new JWTStrategy(services.uaa));
 
 app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.authenticate('JWT', { session: false }));
+// app.use(passport.initialize());
+// app.use(passport.authenticate('JWT', { session: false }));
 
 app.get('/users', function (req, res) {
-  var isAuthorized = req.authInfo.checkLocalScope('Display');
-  if (isAuthorized) {
+  // var isAuthorized = req.authInfo.checkLocalScope('Display');
+  // if (isAuthorized) {
     res.status(200).json(users);
-  } else {
-    res.status(403).send('Forbidden');
-  }
+  // } else {
+  //   res.status(403).send('Forbidden');
+  // }
 });
 
 app.post('/users', function (req, res) {
-  const isAuthorized = req.authInfo.checkLocalScope('Update');
-  if (!isAuthorized) {
-    res.status(403).json('Forbidden');
-    return;
-  }
+  // const isAuthorized = req.authInfo.checkLocalScope('Update');
+  // if (!isAuthorized) {
+  //   res.status(403).json('Forbidden');
+  //   return;
+  // }
 
   var newUser = req.body;
   newUser.id = users.length;
